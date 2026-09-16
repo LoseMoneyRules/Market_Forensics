24f5f3f35cb2832d5f7275b7bc4d3804780ed031
LoseMoneyRules <mutti.marco86@gmail.com>
2026-09-16 13:35:55 -0400

    Register V3.1.12 web engine routes

--- mfapp/__init__.py
+++ mfapp/__init__.py
@@ -69,6 +69,9 @@ def create_app(test_config: dict | None = None) -> Flask:
     from .routes import bp
     app.register_blueprint(bp)
 
+    from .v312_routes import bp as v312_bp
+    app.register_blueprint(v312_bp)
+
     with app.app_context():
         db.create_all()
     return app
